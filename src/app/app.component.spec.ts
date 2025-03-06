a9bbe79f83081b92b9c01ca7a73a02f55aca344b
Adarshpatel0308 <adarshpatel88899@gmail.com>
2025-03-06 16:31:07 +0530

    [Adarsh Patel] add. Display BridgeLabz Logo using property Binding Technique

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'AngularJS' title`, () => {
+  it(`should have the 'AngularJs' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularJS');
+    expect(app.title).toEqual('AngularJs');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularJS');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularJs');
   });
 });
